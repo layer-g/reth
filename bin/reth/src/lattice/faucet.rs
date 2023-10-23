4db45eb80e69087b295e64d2c83008888543d0ab
grantkee <grant@kee.ai>
2023-10-23 17:31:18 -0500

    signing methods and working through test

--- bin/reth/src/lattice/faucet.rs
+++ bin/reth/src/lattice/faucet.rs
@@ -2,16 +2,18 @@
 
 use std::{
     future::Future,
-    time::{UNIX_EPOCH, SystemTime, Duration}, pin::Pin, task::{Context, Poll, ready}
+    time::{UNIX_EPOCH, SystemTime, Duration}, pin::Pin, task::{Context, Poll, ready}, sync::Arc
 };
 
 use futures::StreamExt;
 use jsonrpsee::{proc_macros::rpc, core::async_trait};
 use lru_time_cache::LruCache;
-use reth_primitives::{Address, TxHash, TransactionSigned, FromRecoveredPooledTransaction};
-use reth_rpc::eth::error::{EthResult, EthApiError};
+use reth_primitives::{Address, TxHash, TransactionSigned, FromRecoveredPooledTransaction, ChainSpec, public_key_to_address, U128, U256, Bytes, sign_message, H256, Signature};
+use reth_rpc::eth::error::{EthResult, EthApiError, SignError};
+use reth_rpc_types::{TypedTransactionRequest, LegacyTransactionRequest, TransactionRequest, EIP1559TransactionRequest, TransactionKind};
 use reth_tasks::{TokioTaskExecutor, TaskSpawner};
 use reth_transaction_pool::{TransactionPool, TransactionOrigin};
+use secp256k1::KeyPair;
 use tokio::sync::{mpsc::{UnboundedSender, unbounded_channel, UnboundedReceiver}, oneshot};
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use humantime::format_duration;
@@ -74,6 +76,11 @@ pub struct FaucetConfig {
 
     /// The amount of TEL to transfer to each recipient.
     pub transfer_amount: usize,
+
+    /// The chain id
+    pub chain_id: U64,
+
+    /// 
 }
 
 /// Provides async access to the cached addresses.
@@ -91,17 +98,22 @@ impl FaucetCache {
         config: FaucetConfig,
         pool: Pool,
         executor: Tasks,
+        keypair: KeyPair,
+        nonce: u64,
     ) -> (Self, FaucetCacheService<Pool, Tasks>) {
         let (to_service, rx) = unbounded_channel();
 
         // Construct an `LruCache` of `<String, SystemTime>`s, limited by 24hr expiry time
         let lru_cache = LruCache::with_expiry_duration(config.wait_period);
+        let address = public_key_to_address(keypair.public_key());
         let service = FaucetCacheService {
             request_rx: UnboundedReceiverStream::new(rx),
             pool,
             lru_cache,
             config,
             executor,
+            keypair,
+            nonce,
         };
         let cache = FaucetCache { to_service };
         (cache, service)
@@ -111,11 +123,16 @@ impl FaucetCache {
     /// [tokio::spawn].
     ///
     /// See also [Self::spawn_with]
-    pub fn spawn<Pool>(pool: Pool, config: FaucetConfig) -> Self
+    pub fn spawn<Pool>(
+        pool: Pool,
+        config: FaucetConfig,
+        keypair: KeyPair,
+        nonce: u64,
+    ) -> Self
     where
         Pool: TransactionPool + Unpin + Clone + 'static,
     {
-        Self::spawn_with(pool, config, TokioTaskExecutor::default())
+        Self::spawn_with(pool, config, TokioTaskExecutor::default(), keypair, nonce)
     }
 
     /// Creates a new async LRU backed cache service task and spawns it to a new task via
@@ -126,6 +143,8 @@ impl FaucetCache {
         pool: Pool,
         config: FaucetConfig,
         executor: Tasks,
+        keypair: KeyPair,
+        nonce: u64,
     ) -> Self
     where
         Pool: TransactionPool + Unpin + Clone + 'static,
@@ -135,6 +154,8 @@ impl FaucetCache {
             config,
             pool,
             executor.clone(),
+            keypair,
+            nonce,
         );
 
         executor.spawn_critical("fauce-cache", Box::pin(service));
@@ -146,7 +167,9 @@ impl FaucetCache {
         &self,
         address: Address,
     ) -> EthResult<TxHash> {
-        todo!()
+        let (tx, rx) = oneshot::channel();
+        let _ = self.to_service.send((address, tx));
+        rx.await.map_err(|e| EthApiError::InvalidParams(e.to_string())).and_then(|res| res)
     }
 }
 
@@ -156,6 +179,8 @@ pub struct FaucetCacheService<Pool, Tasks> {
     lru_cache: LruCache<Address, SystemTime>,
     config: FaucetConfig,
     executor: Tasks,
+    keypair: KeyPair,
+    nonce: u64,
 }
 
 impl<Pool, Tasks> FaucetCacheService<Pool, Tasks>
@@ -173,8 +198,34 @@ where
         end.duration_since(SystemTime::now()).map_err(|e| EthApiError::InvalidParams(e.to_string()))
     }
 
-    fn create_transaction(&self, address: Address) -> TransactionSigned {
-        todo!()
+    /// Create a EIP1559 transaction with max fee per gas set to 1 TEL.
+    fn create_transaction(&self, to: Address) -> EthResult<TransactionSigned> {
+        let request = TypedTransactionRequest::EIP1559(EIP1559TransactionRequest {
+            chain_id: self.config.chain_id,
+            nonce: self.nonce.into(),
+            max_priority_fee_per_gas: U128::ZERO,
+            max_fee_per_gas: U128::from(1_000_000_000), // 1 TEL
+            gas_limit: U256::from(1_000_000_000), // 1 TEL
+            kind: TransactionKind::Call(to),
+            value: U256::from(self.config.transfer_amount),
+            input: Default::default(),
+            access_list: Default::default(),
+        });
+        // convert to primitive transaction
+        let transaction = request.into_transaction().ok_or(SignError::InvalidTransactionRequest)?;
+        let tx_signature_hash = transaction.signature_hash();
+        let signature = self.sign_hash(tx_signature_hash)?;
+
+        Ok(TransactionSigned::from_transaction_and_signature(transaction, signature))
+    }
+
+    fn sign_hash(
+        &self,
+        hash: H256,
+    ) -> EthResult<Signature> {
+        let secret = self.keypair.secret_bytes();
+        let signature = sign_message(H256::from_slice(secret.as_ref()), hash);
+        signature.map_err(|e| EthApiError::Signing(SignError::CouldNotSign))
     }
 
 }
@@ -233,4 +284,88 @@ where
     let transaction = <Pool::Transaction>::from_recovered_transaction(recovered.into());
     let hash = pool.add_transaction(TransactionOrigin::Local, transaction).await?;
     Ok(hash)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{sync::Arc, collections::HashMap};
+
+    use reth_blockchain_tree::{BlockchainTree, ShareableBlockchainTree, BlockchainTreeConfig, TreeExternals};
+    use reth_db::test_utils::create_test_rw_db;
+    use reth_interfaces::test_utils::TestConsensus;
+    use reth_primitives::{SealedBlock, BlockBody, ChainSpec, Genesis, GenesisAccount, U256, public_key_to_address};
+    use reth_provider::{ProviderFactory, BlockWriter, providers::BlockchainProvider};
+    use reth_revm::Factory;
+    use reth_transaction_pool::{blobstore::InMemoryBlobStore, TransactionValidationTaskExecutor, PoolConfig};
+    use secp256k1::{rand, KeyPair, Secp256k1};
+    use crate::init::init_genesis;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_request() {
+        // create pool
+        let db = create_test_rw_db();
+        let mut rng = rand::thread_rng();
+        let secp = Secp256k1::new();
+        let keypair = KeyPair::new(&secp, &mut rng);
+        let public = keypair.public_key();
+        println!("\n\npublickey: {:?}\n\n", public);
+
+        let address = public_key_to_address(public);
+        let genesis = genesis(address);
+        let chain = custom_chain(genesis);
+        let factory = ProviderFactory::new(db.as_ref(), Arc::clone(&chain));
+        // let provider = factory.provider_rw().unwrap();
+        let genesis_hash = init_genesis(db.clone(), chain.clone());
+        let consensus = Arc::new(TestConsensus::default());
+
+        // configure blockchain tree
+        let tree_externals = TreeExternals::new(
+            db.clone(),
+            Arc::clone(&consensus),
+            Factory::new(chain.clone()),
+            Arc::clone(&chain),
+        );
+        let tree_config = BlockchainTreeConfig::default();
+        // The size of the broadcast is twice the maximum reorg depth, because at maximum reorg
+        // depth at least N blocks must be sent at once.
+        let (canon_state_notification_sender, _receiver) =
+            tokio::sync::broadcast::channel(tree_config.max_reorg_depth() as usize * 2);
+        let blockchain_tree = ShareableBlockchainTree::new(
+            BlockchainTree::new(
+                tree_externals,
+                canon_state_notification_sender.clone(),
+                tree_config,
+                None,
+            ).unwrap()
+        );
+
+        // setup the blockchain provider
+        let factory = ProviderFactory::new(Arc::clone(&db), Arc::clone(&chain));
+        let blockchain_db = BlockchainProvider::new(factory, blockchain_tree.clone())?;
+        let blob_store = InMemoryBlobStore::default();
+        let validator = TransactionValidationTaskExecutor::eth_builder(Arc::clone(&chain))
+            .with_additional_tasks(1)
+            .build_with_tasks(blockchain_db.clone(), TokioTaskExecutor::default(), blob_store.clone());
+
+        let transaction_pool =
+            reth_transaction_pool::Pool::eth_pool(validator, blob_store, PoolConfig::default());
+
+        let config = FaucetConfig {
+            wait_period: Duration::from_secs(1),
+            transfer_amount: 1,
+        };
+    }
+
+    fn genesis(address: Address) -> Genesis {
+        let accounts = HashMap::from([
+            (address, GenesisAccount::default().with_balance(U256::MAX))
+        ]);
+        Genesis::default().extend_accounts(accounts)
+    }
+
+    fn custom_chain(genesis: Genesis) -> Arc<ChainSpec> {
+        Arc::new(genesis.into())
+    }
 }
